f0a9a76f6d60cb081af6c6625e2dce6463b77565
Eliel <supereliel15@gmail.com>
2017-07-13 15:33:29 -0400

    Historial del empleado

---
+++ resources/views/empleado/historial.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading">HISTORIAL DE EMPLEADO</div>
+          <div class="panel-body">
+            @if($fichas->isNotEmpty())
+              <div class="table-responsive">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Fecha</th>
+                      <th>Horas extras</th>
+                      <th>Hora inicio</th>
+                      <th>Hora fin</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($fichas as $ficha)
+                      <tr>
+                        <th scope="row">{{$loop->iteration}}</th>
+                        <td>{{$ficha->fecha}}</td>
+                        <td>{{$ficha->horas_extras}}</td>
+                        <td>{{$ficha->hora_inicio}}</td>
+                        <td>{{$ficha->hora_fin}}</td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+            @else
+                <div class="alert alert-danger" role="alert">
+                  <p>No se encuentran datos en su historial.</p>
+                </div>
+            @endif
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
