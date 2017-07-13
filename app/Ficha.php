fc4c66d25052d09b6e2eae99650ec2b0e36dd393
Eliel <supereliel15@gmail.com>
2017-07-13 14:40:01 -0400

    Mejoras y relaciones para el modelo ficha

--- app/Ficha.php
+++ app/Ficha.php
@@ -17,6 +17,44 @@ class Ficha extends Model
     protected $dates = [
         'created_at',
         'updated_at',
-        'deleted_at'
+        'deleted_at',
+        'hora_inicio',
+        'hora_fin',
     ];
+
+    public function empleado()
+    {
+        return $this->belongsTo('App\Empleado');
+    }
+
+
+    public function cliente()
+    {
+        return $this->belongsTo('App\Client');
+    }
+
+    public function setHoraInicioAttribute($hora)
+    {
+        if($hora) {
+            $this->attributes['hora_inicio'] =  Carbon::createFromFormat('H:i', $hora);
+        }
+    }
+
+    public function setHoraFinAttribute($hora)
+    {
+        if($hora) {
+            $this->attributes['hora_fin'] = Carbon::createFromFormat('H:i', $hora);
+        }
+    }
+
+    public function getHoraInicioAttribute($hora)
+    {
+        return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+    }
+
+    public function getHoraFinAttribute($hora)
+    {
+        return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+    }
+
 }
