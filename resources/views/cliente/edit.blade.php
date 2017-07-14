7e36c3b981087934dbfdfbdea5ec9eb0ff775c7a
Eliel <supereliel15@gmail.com>
2017-07-13 20:38:51 -0400

    Aumenta tamaño de columna al editar cliente

--- resources/views/cliente/edit.blade.php
+++ resources/views/cliente/edit.blade.php
@@ -3,14 +3,14 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col-md-6 col-md-offset-2">
+      <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
           <div class="panel-heading">EDITAR CLIENTE</div>
           <div class="panel-body">
             {!! Form::open(['url' => '/cliente/'.$cliente->id, 'method' => 'put', 'class' => 'form-inline', 'id' => 'update-cliente-form']) !!}
             <!-- Content form input -->
             <div class="form-group">
-              {!! Form::label('nombre', 'Nombre y apellido:') !!}
+              {!! Form::label('nombre', 'Nombre y Apellido:') !!}
               {!! Form::text('nombre', $cliente->nombre, ['class' => 'form-control', 'required' => 'required']) !!}
             </div>
             <div class="form-group">
