8c08826cb86bcbcf4bba3e92bd9e6cad65ef5412
Eliel <supereliel15@gmail.com>
2017-07-14 20:16:32 -0400

    Modificadores para horas de comida del modelo ficha

--- app/Ficha.php
+++ app/Ficha.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Ficha extends Model
 {
@@ -49,12 +50,44 @@ class Ficha extends Model
 
     public function getHoraInicioAttribute($hora)
     {
-        return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+        if($hora) {
+            return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+        }
     }
 
     public function getHoraFinAttribute($hora)
     {
-        return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+        if($hora) {
+            return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+        }
+    }
+
+        public function setHoraInicioComidaAttribute($hora)
+    {
+        if($hora) {
+            $this->attributes['hora_inicio_comida'] =  Carbon::createFromFormat('H:i', $hora);
+        }
+    }
+
+    public function setHoraFinComidaAttribute($hora)
+    {
+        if($hora) {
+            $this->attributes['hora_fin_comida'] = Carbon::createFromFormat('H:i', $hora);
+        }
+    }
+
+    public function getHoraInicioComidaAttribute($hora)
+    {
+        if($hora) {
+            return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+        }
+    }
+
+    public function getHoraFinComidaAttribute($hora)
+    {
+        if($hora) {
+            return(Carbon::createFromFormat('Y-m-d H:i:s', $hora)->format('H:i'));
+        }
     }
 
 }
