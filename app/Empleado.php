a9004453e23d2aad5cf883e156e00b7800ea0539
Eliel <supereliel15@gmail.com>
2017-07-14 20:48:40 -0400

    Nuevos Metodos del modelo empleado para las politicas de acceso

--- app/Empleado.php
+++ app/Empleado.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Ficha;
+use Carbon\Carbon;
 
 class Empleado extends Model
 {
@@ -30,7 +32,6 @@ class Empleado extends Model
         return $this->belongsTo('App\Empleado');
     }
 
-
     public function user()
     {
         return $this->belongsTo('App\User');
@@ -40,4 +41,71 @@ class Empleado extends Model
     {
         return $this->hasMany('App\Ficha');
     }
+
+    public function jornadaAsignada()
+    {
+        return ($this->jornada_id) ? TRUE : FALSE;
+    }
+
+    public function jornadaAbierta()
+    {
+        $jornada_abierta = Ficha::where('empleado_id',$this->id)
+                         ->where('estado','abierta')->get()->first();
+        return $jornada_abierta ? TRUE : FALSE;
+    }
+
+    public function horaRangoIniciarJornada()
+    {
+        $hora_actual = Carbon::now()->format('H:i');
+        $iniciar_jornada = $this->jornada->hora_inicio_jornada;
+        $fin_jornada = $this->jornada->hora_fin_jornada->subMinute();
+
+        return($hora_actual->between($inicio_jornada,$fin_jornada));
+    }
+
+    public function horaRangoFinalizarJornada()
+    {
+        $hora_actual = Carbon::now()->format('H:i');
+        $fin_jornada = $this->jornada->hora_fin_jornada;
+        $fin_jornada_prorroga = $this->jornada->hora_fin_jornada->addMinutes(30);
+
+        return($hora_actual->between($fin_jornada,$fin_jornada_prorroga));
+    }
+
+    public function jornadaAdmiteHorasExtras()
+    {
+        return $this->jornada->horas_extras ? TRUE : FALSE;
+    }
+
+    public function horaRangoHorasExtras()
+    {
+        $hora_actual = Carbon::now()->format('H:i');
+        $fin_jornada = $this->jornada->hora_fin_jornada;
+        $fin_jornada_prorroga = $this->jornada->hora_fin_jornada->addMinutes(30);
+
+        return($hora_actual->between($fin_jornada,$fin_jornada_prorroga));
+    }
+
+    public function jornadaAdmiteTiempoDescanso()
+    {
+        return $this->jornada->hora_inicio_comida AND $this->jornada->hora_fin_comida;
+    }
+
+    public function horaRangoTiempoDescanso()
+    {
+        $hora_actual            = Carbon::now()->format('H:i');
+        $inicio_tiempo_descanso = $this->jornada->hora_inicio_comida;
+        $fin_tiempo_descanso    = $this->jornada->hora_fin_comida;
+
+        return $hora_actual->between($fin_jornada,$fin_jornada_prorroga);
+    }
+
+    public function tiempoDescansoIniciado()
+    {
+        $ficha = Ficha::where('empleado_id',$this->id)
+                         ->where('estado','abierta')->get()->first();
+
+        return $ficha->hora_inicio_comida ? TRUE : FALSE;
+    }
+
 }
