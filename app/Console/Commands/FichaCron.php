9269874198bf91782751cea1d97382ece5c865d4
Eliel <supereliel15@gmail.com>
2017-07-24 15:36:04 -0400

    Modificado servicio para cerrar jornadas con horas extras

--- app/Console/Commands/FichaCron.php
+++ app/Console/Commands/FichaCron.php
@@ -44,19 +44,27 @@ class FichaCron extends Command
      */
     public function handle()
     {
-        $fichas_abiertas = Ficha::where('estado','abierta')->get();
+        $fichas_abiertas = Ficha::where('estado', 'en progreso')->get();
         $hora_actual = Carbon::now();
-
         foreach($fichas_abiertas as $ficha_abierta) {
             $empleado = $ficha_abierta->empleado;
             $jornada = $empleado->jornada;
             $fin_jornada = Carbon::createFromFormat('H:i', $jornada->hora_fin_jornada);
             if($hora_actual->gt($fin_jornada->addMinutes(30))) {
-                $ficha_abierta->estado = 'cerrada';
+                if($jornada->horas_extras AND $ficha_abierta->hora_inicio_extras) {
+                    $inicio_extras = $ficha_abierta->hora_inicio_extras;
+                    if($hora_actual->gt($inicio_extras->addHours(8))) {
+                        $fin_extras = $ficha_abierta->hora_inicio_extras;
+                        $ficha_abierta->hora_fin_extras = $fin_extras->addHour();
+                        $ficha_abierta->estado = 'cerrado';
+                        $ficha_abierta->save();
+                    }
+                } else {
+                $ficha_abierta->estado = 'cerrado';
                 $ficha_abierta->hora_fin = $jornada->hora_fin_jornada;
                 $ficha_abierta->save();
+                }
             }
         }
-
     }
 }
