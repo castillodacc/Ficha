9bde294c20694cd9a9b8fbfa82b2cdb9ac896150
Eliel <supereliel15@gmail.com>
2017-07-26 00:33:42 -0400

    Seeder cliente

---
+++ database/seeds/ClientesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class ClientesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('clientes')->insert([
+            'nombre' => 'Juana La Cubana',
+            'created_at' => Carbon::now(),
+        ]);
+
+    }
+}
