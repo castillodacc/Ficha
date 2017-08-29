5a81ac1d5ba35552b53f1dee41e6058bc99664e1
Eliel <supereliel15@gmail.com>
2017-08-28 21:36:01 -0400

    Modificado Controlador del reporte para todos los clientes

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -182,9 +182,26 @@ class ReporteController extends Controller
                     }
                 }
             }
+            $clientes = Ficha::select('cliente_id')
+                       ->where('estado', '!=', 'en progreso')
+                       ->whereDate('fecha', '>=', $request->fecha_inicio)
+                       ->whereDate('fecha', '<=', $request->fecha_fin)
+                       ->groupBy('cliente_id')
+                       ->get();
+
+            $fecha_inicio     = $request->fecha_inicio;
+            $fecha_fin        = $request->fecha_fin;
             $horas_trabajadas = $tiempo_trabajado->diff($hora)->format('%H:%i');
             $horas_extras     = $tiempo_extras->diff($hora)->format('%H:%i');
-            $pdf = PDF::loadView('reporte.tabla_clientes', compact('fichas', 'horas_trabajadas', 'horas_extras'));
+            $pdf = PDF::loadView('reporte.tabla_clientes',
+                                 compact('fichas',
+                                         'horas_trabajadas',
+                                         'horas_extras',
+                                         'fecha_inicio',
+                                         'fecha_fin',
+                                         'clientes'
+                                 )
+            );
             return $pdf->stream('reporte.pdf');
         } else {
             return Redirect::back()->withErrors(['Error. No hay información en el rango de fecha seleccionado']);
